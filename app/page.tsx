a6069c445fcbe0533cd734bf9e5869afe46fcb88
Soumik9870 <soumiksaha827@gmail.com>
2025-06-01 16:09:59 +0530

    implement popular companions in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,43 @@
-import { Button } from '@/components/ui/button'
+import CompanionCard from '@/components/CompanionCard'
+import CompanionsList from '@/components/CompanionsList'
+import Cta from '@/components/CTA'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-    <h1 className= "text-2xl underline">Welcome to my Saas App</h1>
-    <Button>
-      Get Started...
-    </Button>
-    </div>
+    <main>
+    <h1>Popular Companions</h1>
+    <section className='home-section'>
+      <CompanionCard 
+        id = "123"
+        name = "Neura the Brainy Explorer"
+        topic= "Neural network of the Brain"
+        subject = "Science"
+        duration = {45}
+        color = "#ffda6e"
+      />
+      <CompanionCard 
+        id = "456"
+        name = "Countsy the Number wizard"
+        topic= "Derivatives and Integrals"
+        subject = "Math"
+        duration = {30}
+        color = "#e5d0ff"
+      />
+      <CompanionCard 
+        id = "789"
+        name = "Verba the vocubulary master"
+        topic= "language and grammar"
+        subject = "English"
+        duration = {45}
+        color = "#bde7ff"
+      />
+    </section>
+    <section className='home-section'>
+      <CompanionsList />
+      <Cta />
+    </section>
+    </main>
   )
 }
 
